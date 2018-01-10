a0ae614f3c0af4b66d4f989c7093b63217bf547e
Corey McCandless <crm1994@gmail.com>
2018-01-10 14:52:40 -0600

    add select function

---
+++ cli.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import argparse
+import json
+from dice import dice
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument('data_file')
+parser.add_argument('--schema', default='schema.json')
+
+opts = parser.parse_args()
+
+with open(opts.data_file) as f:
+    data = json.load(f)
+
+with open(opts.schema, 'r') as f:
+    schema = json.load(f)
+
+print(json.dumps(dice(data, schema)))
